79f1e88ee40448b3842be6d9a2bac5f36926ea52
Noya <qimiaocai011110@gmail.com>
2024-04-11 14:02:47 -0400

    UPDATE

---
+++ RPG-Game/Assets/Scripts/Inventory.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    // Start is called before the first frame update
+    List<Weapon> Weapons;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
