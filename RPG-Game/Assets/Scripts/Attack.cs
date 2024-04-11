dd028bf74f0e477c63d62a4a59d61e2aeb5305f9
Lucas Corey <89759400+ArgentumRhodon@users.noreply.github.com>
2024-04-11 15:24:16 -0400

    goblin follows player within radius

--- RPG-Game/Assets/Scripts/Attack.cs
+++ RPG-Game/Assets/Scripts/Attack.cs
@@ -6,16 +6,35 @@ public class Attack : MonoBehaviour
 {
     private float damage = 10f;
     Rigidbody2D rb;
+    GameObject player;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        player = GameObject.FindFirstObjectByType<PlayerMovement>().gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float sqrDistToPlayer = Vector2.SqrMagnitude(transform.position - player.transform.position);
+        Debug.Log(sqrDistToPlayer);
+
+        if(sqrDistToPlayer < 25)
+        {
+            SeekPlayer();
+            GetComponent<Animator>().SetBool("Run", true);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("Run", false);
+        }
+    }
+
+    void SeekPlayer()
+    {
+        this.transform.position += (player.transform.position - transform.position).normalized * Time.deltaTime;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
