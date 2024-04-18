1083dc7b5f40aef6e1d16cd4ede902e79fefd507
James Zilberman <jaz1324576@gmail.com>
2024-04-18 11:48:31 -0400

    Health bar implemented

---
+++ RPG-Game/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private Image healthBar;
+    [SerializeField] private float fill;
+    // Start is called before the first frame update
+    void Start()
+    {
+        healthBar = this.transform.GetChild(1).GetComponent<Image>();
+    }
+
+    public void UpdateHealthBar(float health, float maxHealth)
+    {
+        healthBar.type = Image.Type.Filled;
+        healthBar.fillMethod = Image.FillMethod.Horizontal;
+        healthBar.fillAmount = health / maxHealth;
+    }
+}
