05a7a5eb5904af55b171f1bfdbbd81f11454534b
urvashi1206 <dhingraurvashi99@gmail.com>
2024-04-22 12:00:08 -0400

    Changes

--- RPG-Game/Assets/Scripts/Player/PlayerControlManager.cs
+++ RPG-Game/Assets/Scripts/Player/PlayerControlManager.cs
@@ -8,11 +8,13 @@ public class PlayerControlManager : MonoBehaviour
     public GameObject archer;
     private Transform playerTransform;
     public GameObject currentPlayer;
+    private PlayerMovement playerMovement;
     // Start is called before the first frame update
     void Start()
     {
         archer.SetActive(false);
         currentPlayer = knight;
+        playerMovement = currentPlayer.GetComponent<PlayerMovement>();
     }
 
     // Update is called once per frame
@@ -25,17 +27,19 @@ public class PlayerControlManager : MonoBehaviour
     {
         if (knight.activeInHierarchy == true)
         {
+            playerMovement.movementVector = Vector2.zero;
             currentPlayer = archer;
             archer.transform.position = playerTransform.position;
-            archer.SetActive(!archer.activeSelf);
-            knight.SetActive(!knight.activeSelf);
+            archer.SetActive(true);
+            knight.SetActive(false);
         }
         else
         {
+            playerMovement.movementVector = Vector2.zero;
             currentPlayer = knight;
             knight.transform.position = playerTransform.position;
-            knight.SetActive(!knight.activeSelf);
-            archer.transform.position = playerTransform.position;
+            knight.SetActive(true);
+            archer.SetActive(false);
         }
     }
 }
