c5ade063b8397e7da45893e712f574f129ec94bd
BoxiFeng9384 <bf9384@g.rit.edu>
2024-04-22 21:11:14 -0400

    Add button

---
+++ RPG-Game/Assets/Scripts/UIButton.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIButton : MonoBehaviour
+{
+    public void EquipItem() 
+    {
+    
+    }
+
+    public void UseItem() 
+    {
+        
+    }
+    public void UnequipItem()
+    {
+        
+    }
+    public void Test() 
+    {
+        Debug.Log("Test");
+    }
+}
