cf000fd2ef9625ff67995fb9e7597b47a72f1d71
Noya <qimiaocai011110@gmail.com>
2024-04-26 22:59:01 -0400

    Revert "Merge branch 'feature_Archer' of https://github.com/ArgentumRhodon/IGME-603-RPG into feature_Archer"
    This reverts commit 46cb7a356a2cbe169bc1d7ad7f4c2b57f6f6c023, reversing
    changes made to c9836ad874c4d8b966c50bdb695fc2d8abed6455.

--- RPG-Game/Assets/Scripts/Player/PlayerControlManager.cs
+++ RPG-Game/Assets/Scripts/Player/PlayerControlManager.cs
@@ -1,22 +1,14 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-
 public class PlayerControlManager : MonoBehaviour
 {
-    public GameObject[] knights;
-    public GameObject[] archers;
-
-    public GameObject knightbasic;
-    public GameObject archerbasic;
+    public GameObject knight;
+    public GameObject archer;
     private Transform playerTransform;
-    private int selectedCharacter = 0;
-
     public GameObject currentPlayer;
-    public GameObject newPlayer;
     private PlayerMovement playerMovement;
     // Start is called before the first frame update
     private static PlayerControlManager _instance;
@@ -35,8 +27,8 @@ public class PlayerControlManager : MonoBehaviour
     }
     void Start()
     {
-        archerbasic.SetActive(false);
-        currentPlayer = knightbasic;
+        archer.SetActive(false);
+        currentPlayer = knight;
         playerMovement = currentPlayer.GetComponent<PlayerMovement>();
     }
 
@@ -45,73 +37,27 @@ public class PlayerControlManager : MonoBehaviour
     {
         if(playerMovement != null)
         {
-            playerTransform = knightbasic.activeSelf ? knightbasic.transform : archerbasic.transform;
+            playerTransform = knight.activeSelf ? knight.transform : archer.transform;
         }
     }
 
     public void OnSwitch()
     {
-        if (knightbasic.activeInHierarchy == true)
+        if (knight.activeInHierarchy == true)
         {
             playerMovement.movementVector = Vector2.zero;
-            currentPlayer = archerbasic;
-            archerbasic.transform.position = playerTransform.position;
-            archerbasic.SetActive(true);
-            knightbasic.SetActive(false);
+            currentPlayer = archer;
+            archer.transform.position = playerTransform.position;
+            archer.SetActive(true);
+            knight.SetActive(false);
         }
         else
         {
             playerMovement.movementVector = Vector2.zero;
-            currentPlayer = knightbasic;
-            knightbasic.transform.position = playerTransform.position;
-            knightbasic.SetActive(true);
-            archerbasic.SetActive(false);
-        }
-    }
-
-    public void OnKnightSwitchType()
-    {
-        if (selectedCharacter == 3)
-        {
-            selectedCharacter = 0;
-        }
-        else
-        {
-            selectedCharacter++;
-        }
-        for (int i = 0; i < 4; i++)
-        {
-            if (i != selectedCharacter)
-            {
-                knights[i].SetActive(false);
-            }
-            else
-            {
-                knights[selectedCharacter].SetActive(true);
-            }
-        }
-    }
-
-    public void OnArcherSwitchType()
-    {
-        if (selectedCharacter == 3)
-        {
-            selectedCharacter = 0;
-        }
-        else
-        {
-            selectedCharacter++;
-        }
-        for (int i = 0; i < 4; i++)
-        {
-            if (i != selectedCharacter)
-            {
-                archers[i].SetActive(false);
-            }
-            else
-            {
-                archers[selectedCharacter].SetActive(true);
-            }
+            currentPlayer = knight;
+            knight.transform.position = playerTransform.position;
+            knight.SetActive(true);
+            archer.SetActive(false);
         }
     }
 }
